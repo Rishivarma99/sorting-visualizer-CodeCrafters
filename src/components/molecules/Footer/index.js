b8eda66dbec4650ab706b5e04814ae2ad2ff97be
Rishi <alluririshivarma@gmail.com>
2024-12-05 08:10:47 +0530

    commit-1

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        <a
+          href="https://www.linkedin.com/in/ramiz-rahman/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Code Crafters
+        </a>
+      </section>
+
+      <section className="Footer__Items">
+
+        <a
+          href="https://github.com/ramiz-rahman/sort-visualizer"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Code
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
